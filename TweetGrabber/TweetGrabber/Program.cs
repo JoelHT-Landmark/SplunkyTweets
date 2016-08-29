13eae92a4c1e36c0a068aabe1563ecc736dd3b43
Rammesses <joel@cheviot-consulting.co.uk>
2016-08-29 15:38:07 +0100

    Created TweetGrabber app

---
+++ TweetGrabber/TweetGrabber/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TweetGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Verbose("About to authenticate...");
+
+            var creds = Authenticate();
+
+            Console.Write("Grab #");
+            var hashtag = Console.ReadLine();
+
+            var tweets = Search.SearchTweets("#" + hashtag);
+
+            logger.Information("Found {matchingTweetCount} tweets for #{searchHashtag}...", tweets.Count(), hashtag);
+            foreach (var tweet in tweets)
+            {
+                logger.Information("{tweetTimestamp} - {twitterUserName} : {tweet}", tweet.CreatedAt, tweet.CreatedBy, tweet.Text);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static ITwitterCredentials Authenticate()
+        {
+            // Create a new set of credentials for the application.
+            var appCredentials = new TwitterCredentials("H0uQO4dVmOTMNyauymkeFe0oM", "kqR5oDC9PSXwosAP24gG76oarN577YVmgv0Y9ZH6u354fd7z3v");
+
+            // Init the authentication process and store the related `AuthenticationContext`.
+            var authenticationContext = AuthFlow.InitAuthentication(appCredentials);
+
+            // Go to the URL so that Twitter authenticates the user and gives him a PIN code.
+            Process.Start(authenticationContext.AuthorizationURL);
+
+            // Ask the user to enter the pin code given by Twitter
+            Console.Write("Twitter PIN >");
+            var pinCode = Console.ReadLine();
+
+            // With this pin code it is now possible to get the credentials back from Twitter
+            var userCredentials = AuthFlow.CreateCredentialsFromVerifierCode(pinCode, authenticationContext);
+
+            // Use the user credentials in your application
+            Auth.SetCredentials(userCredentials);
+
+            return userCredentials;
+        }
+    }
+}
